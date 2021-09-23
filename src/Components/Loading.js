a705cdffb7f68e13a0d28014a5d508cea6c54ce3
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-09-23 20:48:46 -0300

    provider: fetch products

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,6 +1,11 @@
-// import React from 'react';
+import React from 'react';
 
 function Loading() {
+  return(
+      <section>
+        loading
+      </section>
+    )
 }
 
 export default Loading;
--- src/ContextApi/Provider.js
+++ src/ContextApi/Provider.js
@@ -1,10 +1,27 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Context from './Context';
 
 function Provider({ children }) { 
+  const [products, setProducts ] = useState([]);
+  const [isLoading, setisLoading ] = useState(true);
+
+  async function getProducts() {
+    const data = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$camisas');
+    const productsData = await data.json();
+    console.log(productsData.results);
+    setProducts(productsData.results);
+    setisLoading(false);
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
   
-  const contextValue = {};
+  const contextValue = {
+    products,
+    isLoading,
+  };
 
   return (
     <Context.Provider value={ contextValue }>
