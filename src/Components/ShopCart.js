00ec001bbd18d966b4a31587d8c67693f5258d9e
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-09-24 17:26:28 -0300

    sass: shop cart

--- src/Components/ShopCart.js
+++ src/Components/ShopCart.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import Context from '../ContextApi/Context';
-
+import { FiX } from "react-icons/fi";
 
 function ShopCart() {
   const { cart, increment, decrement, remove, total, setshowCart } = useContext(Context);
@@ -10,31 +10,62 @@ function ShopCart() {
   } 
 
   return (
-    <section>
-      
-      <button type="button" onClick={() => setshowCart(false)}> Sair </button>
+    <section className="bg-shopcart">
+      <section className="section-shopcart">
 
-      { cart.map((item) => {
-        const { thumbnail, price, title, amount, id } = item;
-        return (
-          <div key={id} >
-            <img src={ thumbnail } alt="product img" width="100"/>
-            <p> { title } </p>
-            <p> { price } </p>
-            <div>
-              <button type="button" onClick={() => increment(item)}> + </button>
-              <span>{ amount }</span>
-              <button type="button" onClick={() => decrement(item)}> - </button>
+      <div className="header-shopcart">
+        <h2> Sua sacola </h2>
+        <button type="button" onClick={() => setshowCart(false)} className="button"> 
+          <FiX  size={16}/> 
+        </button>
+      </div>
+
+        { cart.map((item) => {
+          const { thumbnail, price, title, amount, id } = item;
+          return (
+            <div key={id}  className="shop">
+
+              <img src={ thumbnail } alt="product img" width="100"/>
+
+              <div className="conteudo">
+                <div className="conteudo-texto">
+                  <p> { title } </p>
+                  <button type="button" onClick={() => remove(item)} className="button"> 
+                    <FiX  size={15}/> 
+                  </button>
+                </div>
+
+                <div className="conteudo-preco">
+
+                  <div className="btn-preco">
+                    <button type="button" onClick={() => increment(item)} className="button"> + </button>
+                      <span>{ amount }</span>
+                    <button type="button" onClick={() => decrement(item)} className="button"> - </button>
+                  </div>
+                 
+
+                  <span> { price } </span>
+                  <p>{ price * amount }</p>
+                </div>
+              </div>
+              
             </div>
-            <p>{ price * amount }</p>
-            <button type="button" onClick={() => remove(item)}> deletar </button>
-          </div>
-        );
-      }) }
+          );
+        }) }
 
-      <p>Total:</p>
-      <span>{ total }</span>
-      
+
+        <div className="finalizar-compra">
+          <div className="total"> 
+            <p>Total: </p>
+            <p>{ total }</p>
+          </div>
+
+          <button type="button" className="button finalizarPedido"> Finalizar pedido </button>
+        </div>
+        
+       
+
+      </section>
     </section>
   );
 }
