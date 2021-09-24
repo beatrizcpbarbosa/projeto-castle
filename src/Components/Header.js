de8505229eee1114e9c9e3eaa281b06bd8d39d01
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-09-24 15:51:40 -0300

    sass: header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,22 @@
 import React, { useContext } from 'react';
 import Context from '../ContextApi/Context';
+import { FiShoppingCart, FiSearch } from "react-icons/fi";
+import logo from '../imgs/logo.svg'
+
 
 function Header() {
-  const { cart } = useContext(Context);
+  const { cart, setshowCart } = useContext(Context);
   return (
-    <section>
-      <p>{cart.length}</p>
-    </section>
+    <header className="header">
+      <FiSearch color="white" size={20}/>
+
+      <img src={logo} alt="logo" />
+
+      <button className="button" onClick={() => setshowCart(true)}>
+        <FiShoppingCart color="white" size={20}/>
+        <div className="amount"> {cart.length} </div>
+      </button>
+    </header>
   );
 }
 
