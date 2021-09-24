421ceacfbfcc853f396164d30b990529a953a5d3
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-09-24 18:33:23 -0300

    sass: carrossel

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,39 +1,30 @@
 import React from 'react';
 import ProductCard from './ProductCard';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+
+
+// import "slick-carousel/slick/slick.css"; 
+// import "slick-carousel/slick/slick-theme.css";
+// import Slider from "react-slick";
+
+import Carousel from 'react-elastic-carousel'
 
 function ProductList({products}) {
 
-  const settings = {
-    arrows: true,
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    adaptiveHeight: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          dots: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          
-        }
-      }]
-  };
-  
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 }
+  ];
+
   return (
-    <section>
+    <section className="produtos">
       <h1>Destaques</h1>
-      <Slider {...settings}>
-       { products.map((product) => <ProductCard key={product.id} product={product} /> ) }
-      </Slider>
-      {/* { products.map((product) => <ProductCard key={product.id} product={product} /> ) } */}
+
+      <Carousel itemsToShow={4} breakPoints={breakPoints}>
+        { products.map((product) => <ProductCard key={product.id} product={product} /> ) }
+      </Carousel>
+      
     </section>
   );
 }
