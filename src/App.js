65705312f9859eb006db01cae0d898ca2385a36d
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-09-24 14:27:08 -0300

    feat: carrossel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import ProviderShop from './ContextApi/Provider'
 import Home from './Pages/Home';
+// import 'slick-carousel/slick/slick.css'; 
+// import 'slick-carousel/slick/slick-theme.css';
 import './App.css';
 
 function App() {
--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import ProductCard from './ProductCard'
+import ProductCard from './ProductCard';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 function ProductList({products}) {
+
+  const settings = {
+    arrows: true,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          dots: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          
+        }
+      }]
+  };
+  
   return (
     <section>
       <h1>Destaques</h1>
-      { products.map((product) => <ProductCard key={product.id} product={product} /> ) }
+      <Slider {...settings}>
+       { products.map((product) => <ProductCard key={product.id} product={product} /> ) }
+      </Slider>
+      {/* { products.map((product) => <ProductCard key={product.id} product={product} /> ) } */}
     </section>
   );
 }
